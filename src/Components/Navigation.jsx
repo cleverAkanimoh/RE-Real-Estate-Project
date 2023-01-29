ce0c011b9650d6cc9f4fcbbb3bd5fe2e43112cae
cleverAkanimoh <crushclever1@gmail.com>
2023-01-29 12:31:54 +0100

    modified 3 files

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,16 +1,19 @@
 import logo from '../assets/logo1.png';
-import '../Stylesheets/navBar.css'
+import "../Stylesheets/navBar.css";
 
-export default function(){
+export default function () {
     return (
-       <nav>
-        <img src={logo} alt="logo" />
-        <ul id="navigation__unordered_list">
-            <li className='active home'>home</li>
-            <li className='landlord'>landlord</li>
-            <li className='tenants'>tenants</li>
-            <li className='contact'>contact us</li>
-        </ul>
-       </nav>
+        <nav>
+            <div className='logo__hamm__container'>
+                <img src={logo} alt="logo" />
+                <button className="hamm__btn"><i className=''>&list;</i></button>
+            </div>
+            <ul id="navigation__unordered_list">
+                <li className='active home'>home</li>
+                <li className='landlord'>landlord</li>
+                <li className='tenants'>tenants</li>
+                <li className='contact'>contact us</li>
+            </ul>
+        </nav>
     )
-   }
+}
