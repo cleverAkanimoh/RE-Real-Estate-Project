087832fc8f853a83defc12ea4709c97b24313db7
cleverAkanimoh <crushclever1@gmail.com>
2023-01-29 12:16:11 +0100

    changed 4 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Navigation from "./Components/Navigation";
+import Header from "./Components/Header";
+import MainContent from "./Components/MainContent";
+import Footer from "./Components/Footer";
+
 export default function App() {
 
   return (
     <main>
-      
+      <Navigation />
+      <Header />
+      <MainContent />
+      <Footer />
     </main>
   )
 };
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,6 +1,15 @@
+import logo from '../assets/logo1.png';
 
 export default function(){
     return (
-       <></>
+       <nav>
+        <img src={logo} alt="logo" />
+        <ul id="navigation__unordered_list">
+            <li className='active home'></li>
+            <li className='active home'></li>
+            <li className='active home'></li>
+            <li className='active home'></li>
+        </ul>
+       </nav>
     )
    }
