7461b334f51f6c7cc67e8602af5672fd90d6d304
cleverAkanimoh <crushclever1@gmail.com>
2023-01-29 12:18:45 +0100

    modified Navigation.jsx in Components

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,14 +1,15 @@
 import logo from '../assets/logo1.png';
+import '../Stylesheets/navBar.css'
 
 export default function(){
     return (
        <nav>
         <img src={logo} alt="logo" />
         <ul id="navigation__unordered_list">
-            <li className='active home'></li>
-            <li className='active home'></li>
-            <li className='active home'></li>
-            <li className='active home'></li>
+            <li className='active home'>home</li>
+            <li className='landlord'>landlord</li>
+            <li className='tenants'>tenants</li>
+            <li className='contact'>contact us</li>
         </ul>
        </nav>
     )
