144a53f703be62bc4a5d70de8ddeaca065435c61
cleverAkanimoh <crushclever1@gmail.com>
2023-02-02 22:29:23 +0100

    modified 4 files

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,21 +3,23 @@ import "../Stylesheets/header.css"
 export default function () {
    return (
       <header>
-         <h2 id="heading__text">the most affordable places to stay in san francisco bay area</h2>
-         <div id="map__filter__div">
-            <picture>
-               <img src='' alt="" />
-            </picture>
-            <div id="filter__div">
-               <select>
-                  <option value="all">all type</option>
-               </select>
-               <select>
-                  <option value="neighborhood">neighborhood</option>
-               </select>
-               <button>sea</button>
+         <section id="header__section">
+            <h2 id="heading__text">the most affordable places to stay in san francisco bay area</h2>
+            <div id="map__filter__div">
+               <picture>
+                  <img src='' alt="" />
+               </picture>
+               <div id="filter__div">
+                  <select>
+                     <option value="all">all type</option>
+                  </select>
+                  <select>
+                     <option value="neighborhood">neighborhood</option>
+                  </select>
+                  <button>sea</button>
+               </div>
             </div>
-         </div>
+         </section>
       </header>
    )
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,23 +1,31 @@
+import { useState } from 'react';
 import logo from '../assets/logo1.png';
 import "../Stylesheets/navBar.css";
 import { VscMenu } from "react-icons/vsc"
 
 export default function () {
+    const [navList, setNavList] = useState(true);
+
+    function navToggle() {
+        setNavList(prevState => !prevState)
+    }
     return (
         <nav>
             <div id="nav__container">
                 <div id='logo__hamm__container'>
                     <img src={logo} alt="logo" />
-                    <button id="hamm__btn">
-                        <VscMenu id='menu__icon'/>
+                    <button id="hamm__btn" onClick={navToggle}>
+                        <VscMenu id='menu__icon' />
                     </button>
                 </div>
-                <ul id="navigation__unordered_list">
-                    <li className='home active'>home</li>
-                    <li className='landlord'>landlord</li>
-                    <li className='tenants'>tenants</li>
-                    <li className='contact'>contact us</li>
-                </ul>
+                {navList &&
+                    <ul id="navigation__unordered_list">
+                        <li className='home active'>home</li>
+                        <li className='landlord'>landlord</li>
+                        <li className='tenants'>tenants</li>
+                        <li className='contact'>contact us</li>
+                    </ul>
+                }
             </div>
         </nav>
     )
