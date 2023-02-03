23f9f3a2c6d73c8a9ee39f8b8e7e20e6170923d8
cleverAkanimoh <crushclever1@gmail.com>
2023-02-03 11:30:47 +0100

    modified MainContent.jsx added mainContent.css

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,10 +1,17 @@
-import swimmingImage from "../assets/swimmingpool.png"
+import { VscHome } from "react-icons/vsc";
+import swimmingImage from "../assets/swimmingpool.png";
+import "../Stylesheets/mainContent.css";
 
 export default function () {
    return (
       <section id="main__content">
          <article id="minimum__article">
-            <img src={swimmingImage} alt="swimming pool" />
+            <h2>minimum living cost takes care of everything</h2>
+            <div id="underline"></div>
+            <div>
+               <img src={swimmingImage} alt="swimming pool" />
+               <aside><VscHome /></aside>
+            </div>
          </article>
       </section>
    )
