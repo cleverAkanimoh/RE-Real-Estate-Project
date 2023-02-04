fccfde13b7d6c0eb573a47e22edb33d0647b0bce
cleverAkanimoh <crushclever1@gmail.com>
2023-02-05 00:48:52 +0100

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ export default function App() {
 
   return (
     <main>
-      {/* <NavBar /> */}
-      {/* <Header /> */}
+      <NavBar />
+      <Header />
       <MainContent />
       <Footer />
     </main>
