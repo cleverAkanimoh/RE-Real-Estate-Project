bbe4fa11642166d682ace12440c54981888eb91c
cleverAkanimoh <crushclever1@gmail.com>
2023-02-05 12:02:41 +0100

    modified Reviews.jsx and mainContent.css

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -10,7 +10,7 @@ export default function () {
                     <p id="reviews__para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel pellentesque est. Curabitur at odio sit amet libero vulputate efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa, faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus fringilla finibus.</p>
                 </div>
                 <div id="reviews__profile">
-                    <img src={profileImage} alt="profile picture" />
+                    <img src={profileImage} alt="profile picture" id="profile__image" />
                     <div id="reviews__profile_info">
                         <big className="colorme">harry wilson</big>
                         <small>property owner</small>
