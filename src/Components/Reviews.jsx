a1c48665a7dc181c6cb7e4145eca81c862f974b6
cleverAkanimoh <crushclever1@gmail.com>
2023-02-05 12:22:49 +0100

    modified Reviews.jsx and mainContent.css

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -4,22 +4,24 @@ import playBtnImage from "../assets/playbtn.png";
 export default function () {
     return (
         <article id="reviews__article">
-            <div id="reviews__info">
-                <div id="reviews__text">
-                    <VscQuote className="colorme quote__icon" />
-                    <p id="reviews__para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel pellentesque est. Curabitur at odio sit amet libero vulputate efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa, faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus fringilla finibus.</p>
-                </div>
-                <div id="reviews__profile">
-                    <img src={profileImage} alt="profile picture" id="profile__image" />
-                    <div id="reviews__profile_info">
-                        <big className="colorme">harry wilson</big>
-                        <small>property owner</small>
+            <div id="reviews__container">
+                <div id="reviews__info">
+                    <div id="reviews__text">
+                        <VscQuote className="colorme quote__icon" />
+                        <p id="reviews__para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel pellentesque est. Curabitur at odio sit amet libero vulputate efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa, faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus fringilla finibus.</p>
+                    </div>
+                    <div id="reviews__profile">
+                        <img src={profileImage} alt="profile picture" id="profile__image" />
+                        <div id="reviews__profile_info">
+                            <big className="colorme">harry wilson</big>
+                            <small>property owner</small>
+                        </div>
                     </div>
                 </div>
+                <aside id="reviews__video_placeholder">
+                    <img src={playBtnImage} alt="video placeholder" />
+                </aside>
             </div>
-            <aside id="reviews__video_placeholder">
-                <img src={playBtnImage} alt="video placeholder" />
-            </aside>
         </article>
     )
 }
