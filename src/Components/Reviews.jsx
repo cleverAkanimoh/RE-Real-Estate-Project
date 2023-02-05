275050fe5afbf58e5325920a4e4f23e152577aa7
cleverAkanimoh <crushclever1@gmail.com>
2023-02-05 11:29:47 +0100

    changed 5 files

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,11 +1,25 @@
-import { } from "react-icons/bs"
-export default function(){
+import { VscQuote } from "react-icons/vsc";
+import profileImage from "../assets/crushpic.png"
+import playBtnImage from "../assets/playbtn.png"
+export default function () {
     return (
         <article id="reviews__article">
-            <div id="reviews__text">
-                <p id="reviews__para"></p>
+            <div id="reviews__info">
+                <div id="reviews__text">
+                    <VscQuote id="quote__icon" />
+                    <p id="reviews__para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel pellentesque est. Curabitur at odio sit amet libero vulputate efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa, faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus fringilla finibus.</p>
+                </div>
+                <div id="reviews__profile">
+                    <img src={profileImage} alt="profile picture" />
+                    <div id="reviews__profile_info">
+                        <big className="colorme">harry wilson</big>
+                        <small>property owner</small>
+                    </div>
+                </div>
             </div>
-            <aside id="reviews__video_placeholder"></aside>
+            <aside id="reviews__video_placeholder">
+                <img src="" alt="video placeholder" />
+            </aside>
         </article>
     )
 }
