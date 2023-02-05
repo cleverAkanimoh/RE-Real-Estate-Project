ad04b02f0e29d11628efaa4057128a07796feb07
cleverAkanimoh <crushclever1@gmail.com>
2023-02-05 03:12:53 +0100

    modified MainContent.jsx and mainContent.css

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -46,12 +46,12 @@ export default function () {
                <h4 className="flexibility__h4 bed">flexible lease</h4>
                <h4 className="flexibility__h4 happy">7-Day happiness quaranteed</h4>
                <h4 className="flexibility__h4 cleaning">monthly house cleaning</h4>
-               <h4 className="flexibility__h4 roommates">choose your roommate</h4>
+               <h4 className="flexibility__h4 roommates">choose your own roommate</h4>
             </div>
             <div id="flexibility__write_up">
                <h2 id="flexibility__h2">Flexibility and options to suit your lifestyle.</h2>
                <p id="flexibility__para">You need it? We got it. We make finding your next home easy, comfortable, and simple. From our happiness guarantee to our selective roommate finder option. We provide you the flexibility that you most desire.</p>
-               <button id="rooms__btn">search rooms</button>
+               <a href="" id="rooms__btn">search rooms</a>
             </div>
          </article>
       </section>
