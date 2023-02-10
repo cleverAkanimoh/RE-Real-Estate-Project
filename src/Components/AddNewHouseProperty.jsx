0c7e34f33e012af16487d2c30ff3690e62d9ebf2
cleverAkanimoh <crushclever1@gmail.com>
2023-02-10 02:31:07 +0100

    auto-commit

--- src/Components/AddNewHouseProperty.jsx
+++ src/Components/AddNewHouseProperty.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function () {
+    const [inputValue, setInputValue] = useState("");
     const [error, setError] = useState("");
     const handleFormSubmit = (e) => {
         const { name, value } = e.target;
